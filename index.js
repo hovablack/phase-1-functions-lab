2f18e7dff85bc845728a5ece112c38fe71713958
Hova Black <nevermind92@hotmail.com>
2021-11-02 06:13:01 -0500

    commit tree d69dba62762ebaae33661789b7f5945447492db7

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(x) {
+  let hq = 42
+  return x > hq ? x - hq : hq - x
+}
+
+function distanceFromHqInFeet(x) {
+  return distanceFromHqInBlocks(x) * 264
+}
+
+function distanceTravelledInFeet(beg, des) {
+  return beg > des ? ((beg - des) * 264) : ((des - beg) * 264)
+}
+
+function calculatesFarePrice(beg, des) {
+  let feet = distanceTravelledInFeet(beg, des)
+  let fare = 0.02
+  let flatFare = 25
+  if (feet < 400) {
+    return feet * 0
+  }else if (feet > 400 && feet < 2000) {
+    return (feet - 400) * fare
+  }else if (feet > 2000 && feet < 2500) {
+    return flatFare
+  }else {
+    return 'cannot travel that far'
+  }
+}
